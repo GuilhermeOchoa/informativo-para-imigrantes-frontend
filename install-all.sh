b7186e43f8880e2912a6083216f76b3297ac5066
giovanni <giovannifrozza470@gmail.com>
2023-08-24 21:55:40 -0300

    arquivo para instalar todas dependencias

---
+++ install-all.sh
@@ -0,0 +1,16 @@
+
+npm install
+# Babel
+npm install --save-dev babel-plugin-module-resolver
+# Fontes
+npx expo install expo-font @expo-google-fonts/roboto
+# Native Base
+npm install native-base
+expo install react-native-svg@12.1.1
+expo install react-native-safe-area-context@3.3.2
+# Stack Navigator
+npm install @react-navigation/native
+npx expo install react-native-screens react-native-safe-area-context
+npm install @react-navigation/native-stack
+# Navegacao por botomTabs
+npm install @react-navigation/bottom-tabs
