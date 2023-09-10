44d7f05f82e001d47360c4d4af12fbf09f0506cf
Pedro Iglesias <pedro.iglesias@edu.pucrs.br>
2023-09-10 18:19:36 -0300

    tirando acesso localhost get

--- src/services/textAPI.ts
+++ src/services/textAPI.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = "http://127.0.0.1:8080/texts/";
+const API_BASE_URL = "http://10.0.2.2:8080/texts/";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
