fe3306b74b11394e122eda2fa2655d47b3bc9f74
arthurtesta93 <arthurtesta@gmail.com>
2023-09-25 11:13:08 -0300

    criação do stylesheet

--- src/screens/RegisterProgramForm.tsx
+++ src/screens/RegisterProgramForm.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, StyleSheet } from 'react-native'
 export function RegisterProgramFormPage() {
 
 
     return (
-            <Text>
-                
+            <Text style={styles.container}>
                 RegisterProgramFormPage
             </Text>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 0,
+        paddingTop: 20,
+        height: 100,
+        width: "100%",
+        alignSelf: "center",
+        alignItems: "center",
+    },
+
+});
