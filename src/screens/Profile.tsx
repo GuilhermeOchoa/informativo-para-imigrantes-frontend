440ccfb913130906f8773a85856a52508f881878
giovanni <giovannifrozza470@gmail.com>
2023-09-30 18:45:30 -0300

    Alterado layout do navbar e adicionado menu de perfil

---
+++ src/screens/Profile.tsx
@@ -0,0 +1,26 @@
+import { VStack, Text, HStack, Center, Divider } from "native-base";
+
+import '@utils/i18n/i18n';
+import { useTranslation } from 'react-i18next';
+
+export function Profile() {
+	const { t, i18n } = useTranslation();
+
+	return (
+		<VStack flex={1} px={6} pb={6} mt={12}>
+			<HStack alignItems="center" m={2}>
+
+				<VStack flex={1}>
+					<Center>
+						<Text fontFamily="body" fontSize="xl" >
+							{t("Profile")}
+						</Text>
+					</Center>
+				</VStack>
+
+			</HStack>
+
+			<Divider my={4} bgColor="green.500" />
+		</VStack>
+	)
+}
