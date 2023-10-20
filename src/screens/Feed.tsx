4963ed52742a8a28c8fa35da812a1563bef260cd
antoniovidalwt <antonio.vidal@poatek.com>
2023-10-20 20:50:43 -0300

    Feed adaptado

--- src/screens/Feed.tsx
+++ src/screens/Feed.tsx
@@ -12,58 +12,33 @@ import { Loading } from '@components/Loading';
 
 import '@utils/i18n/i18n';
 import { useTranslation } from 'react-i18next';
-import { CardArticle } from '@components/CardArticle';
 
 export function Feed() {
     const [isLoading, setIsLoading] = useState(true);
-    const [articles, setArticles] = useState<ArticleDTO[]>([]);
     const navigation = useNavigation<AppNavigatorRoutesProps>()
     const toast = useToast();
 
     const { t, i18n } = useTranslation();
 
-    async function fetchArticles() {
-        try {
-            setIsLoading(true);
-
-            const response = await getArticles(i18n.language);
-            setArticles(response.data);
-        } catch (error) {
-            const isAppError = error instanceof AppError;
-            const title = isAppError ? error.message : t("Nao foi possivel carregar os informativos")
-
-            toast.show({
-                title,
-                placement: "top",
-                bgColor: "red.500"
-            });
-        } finally {
-            setIsLoading(false);
-        }
+    async function fetchFeed() {
+        return oportunidades
     }
 
-    const programas =[{
-        name: "Ensino superior",
-        description: " O programa de ensino de inglês para imigrantes é um programa que visa ensinar inglês para imigrantes",
-        initialDate: "2021-08-01",
-        endDate: "2021-12-01",
-        language: "en",
-        tags: [{ label: "tag1", value: "tag1" }, { label: "tag2", value: "tag2" }, { label: "tag3", value: "tag3" }],
-        link: " https://www.google.com/ ",
-        timeEnrollment: 120,
-        institutionId: 1,
-        status: "APPROVED",
-        timeDuration: 30,
-        minimalRequirements: ["Ser imigrante", "Ter vontade de aprender"],
+    const oportunidades =[{
+        type: "Ensino superior",
+        quantity: 2
+    },
+    {
+        type: "Ensino superior",
+        quantity: 4
+    },
+    {
+        type: "Ensino superior",
+        quantity: 5
     }]
 
-
-    function handleArticlePress(article: ArticleDTO) {
-        navigation.navigate("articleDetails", article);
-    }
-
     useEffect(() => {
-        fetchArticles();
+        fetchFeed();
     }, [i18n.language])
 
     return (
@@ -77,27 +52,15 @@ export function Feed() {
                         </Text>
                     </Center>
                 </VStack>
-
             </HStack>
-            
             <Divider my={4} bgColor="green.500" />
-            
+
             <FlatList
-                    data={programas}
-                    keyExtractor={item => item.name}
+                    data={oportunidades}
                     renderItem={({ item }) => (
                     <OpportunityCads
                         data={item}
-                        cardType="program"
-                        cardContext="adminPrograms"
                         />
-                    )}
-                    ListEmptyComponent={() => (
-                        <VStack flex={1} justifyContent="center" alignItems="center" mt={16}>
-                            <Text fontFamily="body" fontSize="lg">
-                                {t("Nao ha programas disponiveis")}
-                            </Text>
-                        </VStack>
                     )}
                     showsVerticalScrollIndicator={false}
                     _contentContainerStyle={{ paddingBottom: 10 }}
