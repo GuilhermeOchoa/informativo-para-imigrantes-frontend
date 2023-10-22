8dbd28c29952c7c47bab9c6a413254620195d338
fabricio.santos <fabricio.santos002@edu.pucrs.br>
2023-10-22 16:22:39 -0300

    ajeitando detalhes

--- src/screens/FeedCategory.tsx
+++ src/screens/FeedCategory.tsx
@@ -1,99 +1,165 @@
-import React, { useEffect, useState } from 'react';
-import { VStack, Center, FlatList, HStack, useToast, Divider, Text } from 'native-base';
+import React, { useEffect, useState } from "react";
+import {
+  VStack,
+  Center,
+  FlatList,
+  HStack,
+  useToast,
+  Divider,
+  Text,
+} from "native-base";
 
-import { AppError } from '@utils/AppError';
-import { ArticleDTO } from '@dtos/ArticleDTO';
-import { getArticles } from '@services/Articles';
-import { Card } from '@components/Card';
-import { useNavigation } from '@react-navigation/native';
-import { AppNavigatorRoutesProps } from '@routes/app.routes';
+import { AppError } from "@utils/AppError";
+import { AppNavigatorRoutesProps } from "@routes/app.routes";
+import { ArticleDTO } from "@dtos/ArticleDTO";
+import { Card } from "@components/Card";
+import { getArticles } from "@services/Articles";
+import { useNavigation } from "@react-navigation/native";
 
-import { Loading } from '@components/Loading';
+import { Loading } from "@components/Loading";
 
-import '@utils/i18n/i18n';
-import { useTranslation } from 'react-i18next';
-import { CardArticle } from '@components/CardArticle';
-import { ProgramDTO } from '@dtos/ProgramDTO';
-import { getProgramByType } from '@services/Feed';
+import "@utils/i18n/i18n";
+import { useTranslation } from "react-i18next";
+import { CardArticle } from "@components/CardArticle";
+import { ProgramDTO } from "@dtos/ProgramDTO";
+import { getProgramByType } from "@services/Feed";
 
 export function FeedCategory() {
-    const [isLoading, setIsLoading] = useState(true);
-    const [programs, setProgram] = useState<ProgramDTO[]>([]);
-    const navigation = useNavigation<AppNavigatorRoutesProps>()
-    const toast = useToast();
-    const [programType, setProgramType] = "BASIC"
+  const [isLoading, setIsLoading] = useState(true);
+  const [programs, setProgram] = useState<ProgramDTO[]>([]);
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
+  const toast = useToast();
+  const [programType, setProgramType] = "BASIC";
 
-    const { t, i18n } = useTranslation();
+  const { t, i18n } = useTranslation();
 
-    async function fetchPrograns() {
-        try {
-            setIsLoading(true);
+  async function fetchPrograns() {
+    try {
+      setIsLoading(true);
 
-            const response = await getProgramByType(programType);
-            setProgram(response.data);
-        } catch (error) {
-            const isAppError = error instanceof AppError;
-            const title = isAppError ? error.message : t("Nao foi possivel carregar os informativos")
+      const response = await getProgramByType(programType);
+      setProgram(response.data);
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError
+        ? error.message
+        : t("Nao foi possivel carregar os informativos");
 
-            toast.show({
-                title,
-                placement: "top",
-                bgColor: "red.500"
-            });
-        } finally {
-            setIsLoading(false);
-        }
+      toast.show({
+        title,
+        placement: "top",
+        bgColor: "red.500",
+      });
+    } finally {
+      setIsLoading(false);
     }
+  }
 
+  const programas = [
+    {
+      name: "Programa de ensino de inglês para imigrantes",
+      description:
+        " O programa de ensino de inglês para imigrantes é um programa que visa ensinar inglês para imigrantes",
+      initialDate: "2021-08-01",
+      endDate: "2021-12-01",
+      language: "en",
+      tags: [
+        { label: "tag1", value: "tag1" },
+        { label: "tag2", value: "tag2" },
+        { label: "tag3", value: "tag3" },
+      ],
+      link: " https://www.google.com/ ",
+      timeEnrollment: 120,
+      institutionId: 1,
+      status: "APPROVED",
+      timeDuration: 30,
+      minimalRequirements: ["Ser imigrante", "Ter vontade de aprender"],
+    },
+    {
+      name: "Programa de ensino de russo para imigrantes",
+      description:
+        " O programa de ensino de russo para imigrantes é um programa que visa ensinar russo para imigrantes",
+      initialDate: "2021-08-01",
+      endDate: "2021-12-01",
+      language: "en",
+      tags: [
+        { label: "tag1", value: "tag1" },
+        { label: "tag2", value: "tag2" },
+        { label: "tag3", value: "tag3" },
+      ],
+      link: " https://www.google.com/ ",
+      timeEnrollment: 120,
+      institutionId: 1,
+      status: "APPROVED",
+      timeDuration: 30,
+      minimalRequirements: ["Ser imigrante", "Ter vontade de aprender"],
+    },
+    {
+      name: "Programa de ensino de francês para imigrantes",
+      description:
+        " O programa de ensino de francês para imigrantes é um programa que visa ensinar francês para imigrantes",
+      initialDate: "2021-08-01",
+      endDate: "2021-12-01",
+      language: "en",
+      tags: [
+        { label: "tag1", value: "tag1" },
+        { label: "tag2", value: "tag2" },
+        { label: "tag3", value: "tag3" },
+      ],
+      link: " https://www.google.com/ ",
+      timeEnrollment: 120,
+      institutionId: 1,
+      status: "APPROVED",
+      timeDuration: 30,
+      minimalRequirements: ["Ser imigrante", "Ter vontade de aprender"],
+    },
+    {
+      name: "Programa de ensino de espanhol para imigrantes",
+      description:
+        " O programa de ensino de espanhol para imigrantes é um programa que visa ensinar espanhol para imigrantes",
+      initialDate: "2021-08-01",
+      endDate: "2021-12-01",
+      language: "en",
+      tags: [
+        { label: "tag1", value: "tag1" },
+        { label: "tag2", value: "tag2" },
+        { label: "tag3", value: "tag3" },
+      ],
+      link: " https://www.google.com/ ",
+      timeEnrollment: 120,
+      institutionId: 1,
+      status: "APPROVED",
+      timeDuration: 30,
+      minimalRequirements: ["Ser imigrante", "Ter vontade de aprender"],
+    },
+  ];
 
-    // function handleArticlePress(article: ArticleDTO) {
-    //     navigation.navigate("articleDetails", article);
-    // }
+  // function handleArticlePress(article: ArticleDTO) {
+  //     navigation.navigate("articleDetails", article);
+  // }
 
-    useEffect(() => {
-        fetchPrograns();
-    }, [i18n.language])
+  useEffect(() => {
+    fetchPrograns();
+  }, [i18n.language]);
 
-    return (
-		<VStack flex={1} px={6} pb={6} mt={12}>
-			<HStack alignItems="center" m={2}>
-
-				<HStack flex={1} alignItems="center" >
-					
-					<Center flex={1} mr={7}>
-						<Text fontFamily="body" fontSize="xl" >
-							{t("Meus Programas")}
-						</Text>
-					</Center>
-
-                    
-				</HStack>
-
-			</HStack>
-
-			<Divider my={4} bgColor="green.500" />
-			<FlatList
-                    data={programs}
-                    keyExtractor={item => item.name!}
-                    renderItem={({ item }) => (
-                    <Card
-                        data={item}
-                        cardType="program"
-                        cardContext="myPrograms"
-                        />
-                    )}
-                    ListEmptyComponent={() => (
-                        <VStack flex={1} justifyContent="center" alignItems="center" mt={16}>
-                            <Text fontFamily="body" fontSize="lg">
-                                {t("Nao ha programas disponiveis")}
-                            </Text>
-                        </VStack>
-                    )}
-                    showsVerticalScrollIndicator={false}
-                    _contentContainerStyle={{ paddingBottom: 10 }}
-                />
-
-		</VStack>
-	);
+  return (
+    <VStack flex={1} px={6} pb={6} mt={12}>
+      <FlatList
+        data={programas}
+        keyExtractor={(item) => item.name!}
+        renderItem={({ item }) => (
+          <Card data={item} cardType="program" cardContext="myPrograms" />
+        )}
+        ListEmptyComponent={() => (
+          <VStack flex={1} justifyContent="center" alignItems="center" mt={16}>
+            <Text fontFamily="body" fontSize="lg">
+              {t("Nao ha programas disponiveis")}
+            </Text>
+          </VStack>
+        )}
+        showsVerticalScrollIndicator={false}
+        _contentContainerStyle={{ paddingBottom: 10 }}
+      />
+    </VStack>
+  );
 }
-
